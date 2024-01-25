b4b00abaf90580c8b288356c7c7fac9e6870772e
testingpoint09 <testingpoint09@gmail.com>
2024-01-25 19:38:17 +0530

    initial comits by TL

---
+++ src/main/java/com/jan25project_02/App.java
@@ -0,0 +1,13 @@
+package com.jan25project_02;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
