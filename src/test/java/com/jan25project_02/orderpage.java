182e36ed2ed4df5184e4325c929e0f2a0684dedf
testingpoint09 <testingpoint09@gmail.com>
2024-01-26 19:51:32 +0530

    new code commited from eclipse by qa1

---
+++ src/test/java/com/jan25project_02/orderpage.java
@@ -0,0 +1,14 @@
+package com.jan25project_02;
+
+public class orderpage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=10;
+		int n1=20;
+		int m=n1+n;
+
+
+	}
+
+}
