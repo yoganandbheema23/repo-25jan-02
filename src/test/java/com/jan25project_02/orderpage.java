326318e14c2482ce0f3f7f0f73bc961268a9b4b1
yoganandbheema23 <155501138+yoganandbheema23@users.noreply.github.com>
2024-01-26 19:50:14 +0530

    Create orderpage.java
    new file commited from github by qa2

---
+++ src/test/java/com/jan25project_02/orderpage.java
@@ -0,0 +1,14 @@
+package com.jan25project_02;
+public class orderpage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int k=10;
+	int k1=20;
+	int n=n1+n2;
+
+
+
+	}
+
+}
