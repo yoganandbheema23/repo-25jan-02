a710a60f9f663798cd4816aa26464e4540be64d4
testingpoint09 <testingpoint09@gmail.com>
2024-01-29 13:58:45 +0530

    new resetpage.java is added

---
+++ src/test/java/com/jan25project_02/Restpage.java
@@ -0,0 +1,12 @@
+package com.jan25project_02;
+
+public class Restpage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hi..");
+
+	}
+
+}
